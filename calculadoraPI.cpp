9e512646d90de7a4f2ee6caefdc67353a4b68c0e
Everton Souza <118554927+evertonsouz4@users.noreply.github.com>
2023-07-23 08:22:49 -0300

    Create calculadoraPI.cpp

---
+++ calculadoraPI.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+const float PI = 3.1415;
+
+int main() {
+	float r;
+	float perimetro;
+	float area;
+	int opc = 0;
+
+	while (opc != 3) {
+		cout << "## CALCULADORA ##" << "\n"
+			<< "-----------------" << "\n"
+			<< "1 - Perimetro" << "\n"
+			<< "2 - Area" << "\n"
+			<< "3 - Sair" << "\n"
+			<< "-----------------" << "\n"
+			<< "Digite uma opcao (1 / 2 / 3): ";
+		cin >> opc;
+		if (opc == 1) {
+			cout << "\n" << "Digite o raio: ";
+			cin >> r;
+			perimetro = 2 * PI * r;
+			cout << "O perimetro e: " << perimetro << "\n\n";
+		}
+		else if (opc == 2) {
+			cout << "\n" << "Digite o raio: ";
+			cin >> r;
+			area = PI * pow(r, 2);
+			cout << "A area e: " << area << "\n\n";
+		}
+		else if (opc == 3) {
+			cout << "Saindo!!";
+		}
+		else if(opc == 0 || opc >= 4){
+			cout << "ERRO!! Opcao invalida, Digite Novamente!!!" << "\n\n";
+		}
+	}
+		
+	return 0;
+}	
