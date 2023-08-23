786570ea35f591e39e6b4da74ff5e944f0bd90f8
evertonsouz4 <everton.souzaa@outlook.com.br>
2023-08-23 13:23:43 -0300

    Formatando

--- 3-angulo.cpp
+++ 3-angulo.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int quadrado(int n){
+int quadrado(int n) {
     return n * n;
 }
 
-int main(){
+int main() {
     int n1, n2, n3;
-    cout << "Digite 3 numeros:" << "\n";
-    cin>> n1;
-    cin>> n2;
-    cin>> n3;
-    
-    if(n1>0 && n2>0 && n3>0 && 
-      (quadrado(n1) + quadrado(n2) == quadrado(n3) ||
-       quadrado(n2) + quadrado(n3) == quadrado(n1) ||
-       quadrado(n3) + quadrado(n1) == quadrado(n2)) ){
-        cout<<"\n"<<n1 << ", " << n2 << " e " << n3 
-            <<" Podem formar um triangulo com reto!!!" << "\n";
-    }
-    else{
-        cout<<"\n"<<n1<<", "<<n2<<" e "<<n3 
-        <<" Não podem formar um triangulo com reto!!!" << "\n";    
+    cout << "\nDigite 3 numeros:\n";
+    cin >> n1;
+    cin >> n2;
+    cin >> n3;
+
+    if (n1 > 0 && n2 > 0 && n3 > 0 &&
+        (quadrado(n1) + quadrado(n2) == quadrado(n3) ||
+         quadrado(n2) + quadrado(n3) == quadrado(n1) ||
+         quadrado(n3) + quadrado(n1) == quadrado(n2))) {
+        cout << "\nOs numeros " << n1 << ", " << n2 << " e " << n3
+             << " Podem formar um triangulo com angulo reto!!!\n";
+
+    } else {
+        cout << "\n"
+             << n1 << ", " << n2 << " e " << n3
+             << " Nao podem formar um triangulo com angulo reto!!!\n";
     }
 
     return 0;
