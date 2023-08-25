f7117ef6d2169e9c0d75a16567b9d29a38af8198
evertonsouz4 <everton.souzaa@outlook.com.br>
2023-08-24 21:53:56 -0300

    Adicionando nova calculadora

---
+++ 4-fatorial.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int fatorial(int num);
+
+int main(int argc, char *argv[]) {
+    int numero;
+    cout << "\n--------------\n"
+         << "## FATORIAL ##\n"
+         << "--------------\n";
+    cout << "Digite um numero: ";
+    cin >> numero;
+    cout << "A fatorial de " << numero << " = " << fatorial(numero);
+    return 0;
+}
+
+int fatorial(int num) {
+    if (num == 0 || num == 1) {
+        return 1;
+    } else {
+        return num * fatorial(num - 1);
+    }
+}
